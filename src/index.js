b903817956e4158b630d1ca9bb47384c95789373
sanjoyk <sanjoy.sardar@hotstar.com>
2022-12-14 18:13:07 +0530

    feat(random): add ability to get an array of random items
    BREAKING CHANGE: get specified list of random items
    
    close #2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
-var uniqueRamdomArray = require("unique-random-array")
-var starwarsNames = require("./starwars-names.json")
+var uniqueRamdomArray = require("unique-random-array");
+var starwarsNames = require("./starwars-names.json");
+
+var getRandomItem = uniqueRamdomArray(starwarsNames);
 
 module.exports = {
-    all: starwarsNames, 
-    random: uniqueRamdomArray(starwarsNames)
+  all: starwarsNames,
+  random: random,
+};
+
+function random(number) {
+  if (number === undefined) {
+    return getRandomItem();
+  }
+  var randomItems = [];
+  for (let i = 0; i < number; i++) {
+    randomItems.push(getRandomItem());
+  }
+  return randomItems;
 }
